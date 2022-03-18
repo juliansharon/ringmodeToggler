88e212cdc86ea864d1bcbd1f2217b77f875cac74
juliansharon <julianshrn@gmail.com>
2022-03-18 09:45:14 +0530

    initial commit

---
+++ app/src/main/java/com/example/ringmodetoggler/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.ringmodetoggler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+    //add logic to start  the service
+    public void startService(View view) {
+    }
+
+    public void stopService(View view) {
+    }
+}
